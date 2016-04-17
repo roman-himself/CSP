f8da6aa8db7381d5d1b26373221bc4990519945b
Roman Sekuła <roman-himself@o2.pl>
2016-04-18 00:11:26 +0200

    unbuffered channel tests

---
+++ src/test/java/org/romciosoft/csp/ChannelTest.java
@@ -0,0 +1,53 @@
+package org.romciosoft.csp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.romciosoft.io.AsyncAction;
+import org.romciosoft.io.IOActionExecutor;
+
+import java.util.concurrent.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChannelTest {
+    private ScheduledExecutorService scheduledExecutorService;
+    private IOActionExecutor ioActionExecutor;
+
+    @Before
+    public void init() {
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        ioActionExecutor = new IOActionExecutor(scheduledExecutorService);
+    }
+
+    @After
+    public void close() {
+        scheduledExecutorService.shutdown();
+    }
+
+    @Test()
+    public void channelPassing() throws Exception {
+        ChannelHandle<Integer> chHandle = new UnbufferedChannel<Integer>().getHandle();
+        CompletableFuture<Integer> future = new CompletableFuture<>();
+        AsyncAction.fork(chHandle.getSendPort().send(40)).then(
+                chHandle.getReceivePort().receive()
+                        .bind(x -> AsyncAction.wrap(() -> {
+                            future.complete(x + 2);
+                            return null;
+                        }))).getIOAction(ioActionExecutor).perform();
+        assertEquals((Integer) 42, future.get());
+    }
+
+    @Test(expected = TimeoutException.class)
+    public void channelLock() throws Exception {
+        ChannelHandle<Integer> chHandle = new UnbufferedChannel<Integer>().getHandle();
+        CompletableFuture<Integer> future = new CompletableFuture<>();
+        chHandle.getSendPort().send(40)
+                .then(chHandle.getReceivePort().receive()
+                        .bind(x -> AsyncAction.wrap(() -> {
+                            future.complete(x + 2);
+                            return null;
+                        }))).getIOAction(ioActionExecutor).perform();
+        future.get(500, TimeUnit.MILLISECONDS);
+    }
+}
